4aa63885756bb79fe25b2f60ea96315f53daad88
elliot0413 <49327635+elliot0413@users.noreply.github.com>
2020-04-21 16:44:29 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,15 +6,7 @@
 
 const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
 
-if(!process.send) {
 
-Files.initStandalone();
-
-} else {
-
-process.on('message', function(content) {
-	Files.initBotTest(content);
-});
-const token= 'Njk3NzUyODg4ODcyNTM0MDE2.Xp6kCQ.tTQk2SYTo12Lf1vCqwOzqI7PBKU';
+const token= 'Njk3NzUyODg4ODcyNTM0MDE2.Xp6kSA.jhVk89ygGTi1frKxrpCY5X5gDs4';
 bot.login(process.env.token
 }
