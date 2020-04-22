1a0d08bd4f15b14ae0681caa37df8346d1a1f6d6
elliot0413 <49327635+elliot0413@users.noreply.github.com>
2020-04-22 13:40:29 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,5 +2,5 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 import BOT_TOKEN from config
 
-access_token = os.environ["BOT_TOKEN];
+access_token = os.env["BOT_TOKEN];
 bot.login(access_token);
