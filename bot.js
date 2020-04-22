2f92cfc36c02c1f083b08e1b1c4864681481a662
elliot0413 <49327635+elliot0413@users.noreply.github.com>
2020-04-22 13:50:12 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,19 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-import os
+/*
+ * Discord Bot Builder Bot
+ * Version 1.2.0
+ * Robert Borghese
+ */
 
-access_token = os.env["BOT_TOKEN];
-bot.login(access_token);
+const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+
+if(!process.send) {
+
+Files.initStandalone();
+
+} else {
+
+process.on('message', function(content) {
+	Files.initBotTest(content);
+});
+
+}
